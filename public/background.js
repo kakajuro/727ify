17b4dc97e9536c8716203b2b287cd75415cd9566
David F <Skilliciousboy@icloud.com>
2020-12-13 08:52:32 +0000

    cleaned up code

--- public/background.js
+++ public/background.js
@@ -3,11 +3,6 @@ chrome.extension.onMessage.addListener(
         if (request.action == "convertPage") {
             chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
                 chrome.tabs.executeScript(null, {file: "content.js"});
-                // console.log("rec1")
-                // console.log(tab[0].id);
-                // let msg = "go";
-                // chrome.tabs.sendMessage(tab[0].id,msg);
-                // console.log("recieved");
               });
 
         }
--- public/popup.js
+++ public/popup.js
@@ -6,7 +6,7 @@ function sendMessage(){
 
 document.addEventListener('DOMContentLoaded', function() {
 var link = document.getElementById('mainButton');
-// onClick's logic below:
+
 link.addEventListener('click', function() {
     sendMessage();
 });
